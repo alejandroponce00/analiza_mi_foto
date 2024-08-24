e2f91163f41c505e2961e376d4362b5691b16183
Alejandro Ponce <122680586+alejandroponce00@users.noreply.github.com>
2024-08-24 20:55:03 -0300

    segundo commit

--- src/app/api/upload/route.js
+++ src/app/api/upload/route.js
@@ -1,23 +1,38 @@
 import { writeFile } from "fs/promises";
+import { NextResponse } from "next/server";
 import path from "path";
 
 
 export async function POST(request) {
 
-  const data = await request.formData();
-  const file = data.get("file");
+  try {const data = await request.formData();
+    const file = data.get("file");
+  
+    
+    const bytes = await file.arrayBuffer();
+    const buffer = Buffer.from(bytes);
+  
+    const filePath = path.join(process.cwd(), 'public/imagenes_subidas', file.name);
+  
+    writeFile(filePath, buffer);
+    console.log("archivo guardado en ", filePath);
+  
+    return new Response(
+      JSON.stringify({
+        message: "Archivo subido correctamente",
+      })
+    );
+    
+  } catch (error) {
+    return NextResponse.json(
+      JSON.stringify({
+      message:"no subiste el archivo",
 
-  const bytes = await file.arrayBuffer();
-  const buffer = Buffer.from(bytes);
-
-  const filePath = path.join(process.cwd(), 'public/imagenes_subidas', file.name);
-
-  writeFile(filePath, buffer);
-  console.log("archivo guardado en ", filePath);
-
-  return new Response(
-    JSON.stringify({
-      message: "Archivo subido correctamente",
-    })
+    }) ,
+    {
+      status:400,
+    }
   );
+    
+  }
 }
