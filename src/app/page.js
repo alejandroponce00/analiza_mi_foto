83631eaa0f53f7362b804fac11d48bd8c81d7362
Alejandro Ponce <122680586+alejandroponce00@users.noreply.github.com>
2024-08-29 16:30:31 -0300

    estilizado

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,8 @@ function HomePage() {
 const [file, setFile] = useState();
 
   return (
-    <div>
+    <div className="flex h-screen justify-center items-center">
+    
       <form onSubmit={async(e) =>{
         e.preventDefault()
         if (!file) return 
@@ -19,14 +20,17 @@ const [file, setFile] = useState();
        })
        const data = await res.json()
        console.log(data)
-      }}>
-        <label>Subir Archivo:</label>
-        <input type="file"
+      }} className="bg-zinc-800 p-5 ">
+        <h1 className="text-center text-4xl my-4">Subir archivo</h1>
+        
+        <input type="file"className="bg-zinc-900 text-zinc-100 p-2 rounded block mb-2"
         onChange={(e)=>{
            setFile(e.target.files[0])
-        }}
+        }} 
         />
-        <button className="bg-white text-black">Subir</button>
+        <button className="bg-green-500 text-zinc-100 text-xl p-1 rounded-sm block w-full disabled:opacity-50"
+        disabled={!file}
+       >Subir</button>
       </form>
     </div>
   );
